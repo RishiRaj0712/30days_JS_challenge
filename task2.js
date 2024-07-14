742c4a27ac29636f0475ac99fb23f640f159913e
Rishi Raj <rishiraj.rr01@gmail.com>
2024-07-14 08:46:00 +0530

    day2 done

---
+++ task2.js
@@ -0,0 +1,26 @@
+let var1 = 10;
+let var2 = 6.9;
+let var3 = true;
+let var4 = false;
+
+console.log(var1 + var2);
+console.log(var1 - var2);
+console.log(var1 * var2);
+console.log(var2 / var1);
+console.log(var2 % var1);
+
+let var5 =21;
+console.log(var5 += (var1 * var2));
+console.log(var5 -= (var1 + var2));
+
+console.log(var5 > var1);
+console.log(var1<var5);
+
+console.log(5<=5);
+console.log(4>=5);
+
+console.log(var3 && var4);
+console.log(var4 || var3);
+console.log(!var3);
+
+console.log((var2 + var1)<0 ? "Negative" : "Non-Negative");
