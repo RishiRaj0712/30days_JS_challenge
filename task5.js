b6f3d1b163f78f97f180c2f08f447cd7b001a990
Rishi Raj <rishiraj.rr01@gmail.com>
2024-07-17 14:37:32 +0530

    issue fixed

--- task5.js
+++ task5.js
@@ -23,14 +23,12 @@ func3(-12,-43)
 const func4 = (str1, str2)=>{
     return str1 + str2;
 }
-
 console.log(func4("hello"," world"));
 
 const func5 = (num5, num6)=>{
     return num5 + num6;
 }
 console.log(func5(4,7));
-
 
 const ch = 'r';
 const func6 = (str1) =>{
@@ -41,13 +39,11 @@ const func6 = (str1) =>{
 }
 console.log(func6("aishi"));
 
-
 const func7 =(num6,num7=5)=>{
     return num6*num7;
 }
 console.log(func7(3,7));
 console.log(func7(3));
-
 
 const func8 = (name, age= 18)=>{
     return `Hi ${name} !. Welcome to ${age}+ club. `
